e6d53f047f5ea370fcfbba5f6d0212a765238734
bbg <bagbrc@gmail.com>
2018-10-29 14:21:39 +0300

    Entity Class

---
+++ Bölüm 15 - Entity Framework/EntitiyFrameworkUygulama/EntitiyFrameworkUygulama/Kategori.cs
@@ -0,0 +1,14 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Text;
+using System.Threading.Tasks;
+
+namespace EntitiyFrameworkUygulama
+{
+    public class Kategori
+    {
+        public int Id { get; set; }
+        public string kategoriAdi { get; set; }
+    }
+}
---
+++ Bölüm 15 - Entity Framework/EntitiyFrameworkUygulama/EntitiyFrameworkUygulama/Program.cs
@@ -0,0 +1,31 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Text;
+using System.Threading.Tasks;
+
+namespace EntitiyFrameworkUygulama
+{
+    class Program
+    {
+        static void Main(string[] args)
+        {
+            List<Kategori> kategori = new List<Kategori>()
+            {
+                new Kategori(){Id=1,kategoriAdi="Telefon"},
+                new Kategori(){Id=2,kategoriAdi="Bilgisayar"},
+                new Kategori(){Id=3,kategoriAdi="Tablet"}
+            };
+
+
+            List<Urun> urun = new List<Urun>()
+            {
+                new Urun(){Id=1, urunAdi="Iphone 7", Fiyat=3500.0,stok=1000},
+                new Urun(){Id=2, urunAdi="Iphone 6", Fiyat=1500.0,stok=10},
+                new Urun(){Id=3, urunAdi="SAmsung S-7", Fiyat=3000.0,stok=2000},
+                new Urun(){Id=4, urunAdi="Samsung Note3", Fiyat=2700,stok=0},
+                new Urun(){Id=5, urunAdi="Iphone x", Fiyat=15500.0,stok=1000},
+            };
+        }
+    }
+}
