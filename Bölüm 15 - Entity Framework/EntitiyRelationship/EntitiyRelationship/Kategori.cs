c0b7e49bb812ee0c2fd72a10f7870126520dbf72
bbg <bagbrc@gmail.com>
2018-10-30 13:49:13 +0300

    Entitiy RelationShips

---
+++ Bölüm 15 - Entity Framework/EntitiyRelationship/EntitiyRelationship/Kategori.cs
@@ -0,0 +1,16 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Text;
+using System.Threading.Tasks;
+
+namespace EntitiyRelationship
+{
+    public class Kategori
+    {
+        public int Id { get; set; }
+        public string kategoriAdi { get; set; }
+
+        public List<Urun> Urunler { get; set; } // Program anında kullanılacak Db de olusmayacak
+    }
+}
---
+++ Bölüm 15 - Entity Framework/EntitiyRelationship/EntitiyRelationship/Urun.cs
@@ -0,0 +1,22 @@
+﻿using System;
+using System.Collections.Generic;
+using System.ComponentModel.DataAnnotations;
+using System.Linq;
+using System.Text;
+using System.Threading.Tasks;
+
+namespace EntitiyRelationship
+{
+    public class Urun
+    {
+        [Key]
+        public int urunId { get; set; }
+        public String urunAdi { get; set; }
+        public double Fiyat { get; set; }
+        public int stok { get; set; }
+
+        public int KategoriId { get; set; }  // Sınıf adı + Id (Arada boşluk olmayack.O otomatik olarak onu fk olarak algılıyor.Tablolar bagşanmıs oldu
+        public Kategori Kategori { get; set; } // Veritabanında olusmayacak sadece program anında kullanıcaz.
+        public List<Tedarikci> Tedarikciler { get; set; }
+    }
+}
