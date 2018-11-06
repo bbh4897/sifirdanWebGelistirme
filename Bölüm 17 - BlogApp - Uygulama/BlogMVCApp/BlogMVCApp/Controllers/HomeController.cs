6f4bd8038ee8cbb1ac511117dd4fb066ed52cd9e
bbg <bagbrc@gmail.com>
2018-11-06 19:17:41 +0300

    Anasayfa Blog Listesi

--- Bölüm 17 - BlogApp - Uygulama/BlogMVCApp/BlogMVCApp/Controllers/HomeController.cs
+++ Bölüm 17 - BlogApp - Uygulama/BlogMVCApp/BlogMVCApp/Controllers/HomeController.cs
@@ -14,7 +14,18 @@ namespace BlogMVCApp.Controllers
         // GET: Home
         public ActionResult Index()
         {
-            return View(context.Bloglar.ToList());
+            var bloglar = context.Bloglar.Select(i=> new BlogModel()
+            {
+                Id=i.Id,
+                Baslik=i.Baslik.Length>100?i.Baslik.Substring(0,10) + "..." : i.Baslik,
+                Resim = i.Resim,
+                EklenmeTarihi = i.EklenmeTarihi,
+                Onay = i.Onay,
+                Anasayfa = i.Anasayfa
+            })
+            .Where(i => i.Onay == true && i.Anasayfa == true);
+            return View(bloglar.ToList());
         }
     }
 }
+
--- Bölüm 17 - BlogApp - Uygulama/BlogMVCApp/BlogMVCApp/Models/BlogInitializer.cs
+++ Bölüm 17 - BlogApp - Uygulama/BlogMVCApp/BlogMVCApp/Models/BlogInitializer.cs
@@ -27,13 +27,13 @@ namespace BlogMVCApp.Models
             List<Blog> bloglar = new List<Blog>()
             {
                new Blog() {Baslik="C# Delegates Hakkında", Aciklama="Acıklama 1",EklenmeTarihi=DateTime.Now.AddDays(-10), Anasayfa=true, Onay=true, Icerik="Icerık 1", Resim="1.jpg", CategoryId=1},
-               new Blog() {Baslik="Generic List Hakkında", Aciklama="Acıklama 2",EklenmeTarihi=DateTime.Now.AddDays(-30), Anasayfa=false, Onay=true, Icerik="Icerık 2", Resim="2.jpg", CategoryId=2},
+               new Blog() {Baslik="Generic List Hakkında", Aciklama="Acıklama 2",EklenmeTarihi=DateTime.Now.AddDays(-30), Anasayfa=true, Onay=true, Icerik="Icerık 2", Resim="2.jpg", CategoryId=2},
                new Blog() {Baslik="Asp.Net Hakkında", Aciklama="Acıklama 3",EklenmeTarihi=DateTime.Now.AddDays(-10), Anasayfa=false, Onay=false, Icerik="Icerık 3", Resim="3.jpg", CategoryId=3},
                new Blog() {Baslik="Java Delegates Hakkında", Aciklama="Acıklama 4",EklenmeTarihi=DateTime.Now.AddDays(-40), Anasayfa=true, Onay=false, Icerik="Icerık 4", Resim="4.jpg", CategoryId=4},
-               new Blog() {Baslik="Android Delegates Hakkında", Aciklama="Acıklama 5",EklenmeTarihi=DateTime.Now.AddDays(-1), Anasayfa=true, Onay=false, Icerik="Icerık 5", Resim="5.jpg", CategoryId=1},
+               new Blog() {Baslik="Android Delegates Hakkında", Aciklama="Acıklama 5",EklenmeTarihi=DateTime.Now.AddDays(-1), Anasayfa=true, Onay=true, Icerik="Icerık 5", Resim="5.jpg", CategoryId=1},
                new Blog() {Baslik="Veritabanı Delegates Hakkında", Aciklama="Acıklama 6",EklenmeTarihi=DateTime.Now.AddDays(-10), Anasayfa=false, Onay=true, Icerik="Icerık 6", Resim="6.jpg", CategoryId=3},
                new Blog() {Baslik="Sistem Programlama Delegates Hakkında", Aciklama="Acıklama 7",EklenmeTarihi=DateTime.Now.AddDays(-20), Anasayfa=true, Onay=false, Icerik="Icerık 7", Resim="7.jpg", CategoryId=4},
-               new Blog() {Baslik="Ayrık Matematik Delegates Hakkında", Aciklama="Acıklama 8",EklenmeTarihi=DateTime.Now.AddDays(-10), Anasayfa=false, Onay=true, Icerik="Icerık 8", Resim="8.jpg", CategoryId=2}
+               new Blog() {Baslik="Ayrık Matematik Delegates Hakkında", Aciklama="Acıklama 8",EklenmeTarihi=DateTime.Now.AddDays(-10), Anasayfa=true, Onay=true, Icerik="Icerık 8", Resim="8.jpg", CategoryId=2}
             };
 
             foreach (var item in bloglar)
