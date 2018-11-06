20e8999df3c22af47d2e2ecf0f6c91a349460d0b
bbg <bagbrc@gmail.com>
2018-11-06 14:02:49 +0300

    Entity Sınıfları

---
+++ Bölüm 17 - BlogApp - Uygulama/BlogMVCApp/BlogMVCApp/Models/Category.cs
@@ -0,0 +1,17 @@
+﻿using System;
+using System.Collections.Generic;
+using System.ComponentModel.DataAnnotations;
+using System.Linq;
+using System.Web;
+
+namespace BlogMVCApp.Models
+{
+    public class Category
+    {
+        [Key]
+        public int Id { get; set; }
+        public string kategoriAdi { get; set; }
+
+        public List<Blog> Bloglar { get; set; }
+    }
+}
