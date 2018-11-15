2538f73505208225ee1a30c2348e88cb52eae633
bbg <bagbrc@gmail.com>
2018-11-15 16:56:40 +0300

    Urun Details

---
+++ Bölüm 13 - ASP.NET MVC  Orta Düzey/Genel Uygulama/gu_UrunDetails/gu_UrunDetails/Controllers/HomeController.cs
@@ -0,0 +1,34 @@
+﻿using gu_UrunDetails.Models;
+using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Web;
+using System.Web.Mvc;
+
+namespace gu_UrunDetails.Controllers
+{
+    public class HomeController : Controller
+    {
+        List<Urun> urun = new List<Urun>();
+        // GET: Home
+        public ActionResult Index()
+        {
+           
+            urun = Veritabani.List;
+            return View(urun);
+        }
+
+        public ActionResult Details(int Id)
+        {
+            Urun entity=null;
+            foreach (var item in Veritabani.List)
+            {
+                if (item.urunId == Id)
+                {
+                    entity = item;
+                }
+            }
+            return View(entity);
+        }
+    }
+}
---
+++ Bölüm 13 - ASP.NET MVC  Orta Düzey/Genel Uygulama/gu_UrunDetails/gu_UrunDetails/Global.asax
@@ -0,0 +1,1 @@
+﻿<%@ Application Codebehind="Global.asax.cs" Inherits="gu_UrunDetails.MvcApplication" Language="C#" %>
