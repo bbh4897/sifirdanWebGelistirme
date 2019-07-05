9cbef892dc3a12771077e62aaf854e59972d6267
bbg <bagbrc@gmail.com>
2019-07-05 16:18:55 +0300

    tema giydirme / navbar

---
+++ AtilimElektrik/AtilimElektrik/AtilimElektrik/Controllers/HomeController.cs
@@ -0,0 +1,37 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Web;
+using System.Web.Mvc;
+
+namespace AtilimElektrik.Controllers
+{
+    public class HomeController : Controller
+    {
+        // GET: Home
+        public ActionResult Index()
+        {
+            return View();
+        }
+
+        public ActionResult Hakkimizda()
+        {
+            return View();
+        }
+
+        public ActionResult Hizmetlerimiz()
+        {
+            return View();
+        }
+
+        public ActionResult Urunlerimiz()
+        {
+            return View();
+        }
+
+        public ActionResult Iletisim()
+        {
+            return View();
+        }
+    }
+}
---
+++ AtilimElektrik/AtilimElektrik/AtilimElektrik/Global.asax
@@ -0,0 +1,1 @@
+﻿<%@ Application Codebehind="Global.asax.cs" Inherits="AtilimElektrik.MvcApplication" Language="C#" %>
